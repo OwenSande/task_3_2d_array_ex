6f442e3f0eba54c3aad53bd53a6ce3e6759033c3
OwenSande <owensande07@gmail.com>
2022-03-31 23:03:50 +0200

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,28 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    int n = 10, i, j, sum =0;
+        int matrix[][] = new int [n][n];
+
+        //entering number from 0 to 9 in the table
+        for (i =0; i < matrix.length; i++){
+
+            for(j = 0; j< matrix.length; j++){
+
+                if(i == j){
+                    matrix[i][j] =i;
+                    sum += matrix[i][j];
+
+                }else{
+                    matrix[i][j] = 0;
+                }
+                System.out.println(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+        System.out.println("The sum of the highlighted elements in the array is equal to: " + sum);
+    }
+}
